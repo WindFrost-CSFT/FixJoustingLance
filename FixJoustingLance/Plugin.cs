90b28faf65251c694e80b5ed928488e5a8b602be
Cai <13110818005@qq.com>
2026-02-14 17:37:35 +0800

    fix: use TryDismount OnHook

--- FixJoustingLance/Plugin.cs
+++ FixJoustingLance/Plugin.cs
@@ -11,33 +11,26 @@ public class Plugin(Main game) : TerrariaPlugin(game)
     public override string Name => "FixJoustingLance";
     public override string Author => "Cai";
     public override string Description => "修复骑枪PVP报错不扣血 (PE)";
-    public override Version Version => new(1, 0);
+    public override Version Version => new(1, 1);
 
     public override void Initialize()
     {
-        Mount.CanDismount += MountOnCanDismount;
-        Mount.CanDismountWithResult += Mount_CanDismountWithResult;
+        Mount.TryDismount += MountOnTryDismount;
     }
+    
 
-    private Terraria.Mount.DismountCheckResult Mount_CanDismountWithResult(Mount.orig_CanDismountWithResult orig, Terraria.Mount self, Player mountingPlayer)
+    private static bool MountOnTryDismount(Mount.orig_TryDismount orig, Terraria.Mount self, Player mountedPlayer)
     {
-        if (self.Type == -1)
-            return Terraria.Mount.DismountCheckResult.FailedCCed;
-        return orig(self, mountingPlayer);
+        return self.Type != -1 && orig(self, mountedPlayer);
     }
-
-    private static bool MountOnCanDismount(Mount.orig_CanDismount orig, Terraria.Mount self, Player mountingPlayer)
-    {
-        return self.Type != -1 && orig(self, mountingPlayer);
-    }
+    
 
 
     protected override void Dispose(bool disposing)
     {
         if (disposing)
         {
-            Mount.CanDismount -= MountOnCanDismount;
-            Mount.CanDismountWithResult -= Mount_CanDismountWithResult;
+            Mount.TryDismount -= MountOnTryDismount;
         }
     }
 }
